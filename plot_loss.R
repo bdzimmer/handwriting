b76b0c17c4693c08b28bd9a6bac30ae3af2ee4c9
Ben Zimmer <ben.d.zimmer@gmail.com>
2017-12-28 09:58:00 -0600

    visualize validation scores during training

--- plot_loss.R
+++ plot_loss.R
@@ -2,17 +2,13 @@
 
 # Plot loss and related measurements for monitoring deep learning.
 
-inputFilename <- "log.txt"
+lossInputFilename <- "log_charpos.txt"
+callbackInputFilename <- "log_charpos_callback.txt"
 
 
-plotLoss <- function(inputFilename) {
-  d <- read.csv(inputFilename, header = FALSE)
-  
-  colnames(d) <- c("epoch", "loss", "grad", "time")
-  
-  par(mfrow=c(2, 1))
+plotLoss <- function(d) {
   with(d, {
-    plot(loss ~ epoch, type = "l", lwd = 2, col = "orange")
+    plot(loss ~ epoch, type = "l", lwd = 2, col = "red")
     title(main = paste(
       "Loss",
       round(tail(loss, 1), 6)))
@@ -27,12 +23,39 @@ plotLoss <- function(inputFilename) {
 }
 
 
+plotGeneric <- function(d) {
+  # assumes first column of d is epoch
+  ncols <- ncol(d) - 1
+  for(idx in 2:(ncols + 1)) {
+    curData <- d[[idx]]
+    curDataName <- colnames(d)[idx]
+    plot(
+      curData ~ d$epoch, type = "l", lwd = 2, col = "green",
+      xlab = "epoch", ylab = curDataName)
+    title(main = paste(
+      curDataName,
+      round(tail(curData, 1), 6)))
+  }
+}
+
+
 while(TRUE) {
   tryCatch({
-    plotLoss(inputFilename)
+    
+    par(mfcol=c(3, 2))
+    
+    d <- read.csv(lossInputFilename, header = FALSE)
+    colnames(d) <- c("epoch", "loss", "grad", "time")
+    plotLoss(d)
+    plot(0, type="n")
+    
+    d <- read.csv(callbackInputFilename, header = FALSE)
+    colnames(d) <- c("epoch", "accuracy", "roc_auc", "distance")
+    plotGeneric(d)
+    
   },
   error = function(e) {
-    cat("error reading file\n")
+    cat("error reading files or plotting\n")
   })
   Sys.sleep(15)
 }
