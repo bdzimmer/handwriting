312837fbdcc5b70803a3002cd2d98c85f809a44c
Ben Zimmer <ben.d.zimmer@gmail.com>
2018-03-06 19:32:36 -0600

    update data module

--- handwriting/data.py
+++ handwriting/data.py
@@ -9,45 +9,19 @@ Manage training and test pages.
 
 
 SAMPLE_FILENAMES = (
-    ["data/20170929_" + str(idx) + ".png.sample.pkl.1" # 0-4
+    ["data/20170929_" + str(idx) + ".png.sample.pkl.1"  # 0-4
      for idx in range(1, 6)] +
-    ["data/20171120_" + str(idx) + ".png.sample.pkl.1" # 5-8
+    ["data/20171120_" + str(idx) + ".png.sample.pkl.1"  # 5-8
      for idx in range(1, 5)] +
-    ["data/20171209_" + str(idx) + ".png.sample.pkl.1" # 9-12
+    ["data/20171209_" + str(idx) + ".png.sample.pkl.1"  # 9-12
      for idx in range(1, 5)] +
     ["data/20180109_" + str(idx) + ".png.sample.pkl.1"  # 13-15
-     for idx in range(1, 4)]
+     for idx in range(1, 4)] +
+    ["data/20180224_1.png.sample.pkl.1"]  # 16
 )
 
 
 def pages(idxs):
     """get lists of filenames given idxs of pages"""
-
-    filenames = [SAMPLE_FILENAMES[idx] for idx in idxs]
+    return [SAMPLE_FILENAMES[idx] for idx in idxs]
     return filenames
-
-
-# def train_test_pages(test_idxs):
-#
-#     """Get lists of training and test pages given idxs of test pages."""
-#
-#     train_idxs = [
-#         idx for idx in range(len(SAMPLE_FILENAMES))
-#         if idx not in test_idxs]
-#
-#     return pages(train_idxs, test_idxs)
-
-
-# def pages(train_idxs, test_idxs):
-#
-#     train_filenames = [
-#         SAMPLE_FILENAMES[idx]
-#         for idx in range(len(SAMPLE_FILENAMES))
-#         if idx in train_idxs]
-#
-#     test_filenames = [
-#         SAMPLE_FILENAMES[idx]
-#         for idx in range(len(SAMPLE_FILENAMES))
-#         if idx in test_idxs]
-#
-#     return train_filenames, test_filenames
